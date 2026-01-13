c9d74e60414c5330a45d2565fe83cb44431c4ddb
Yuhaojjjj <ye.yuh24@zaragoza.salesianos.edu>
2026-01-13 11:50:08 +0100

    Add files via upload

---
+++ cuatreador_media.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+	float arr[4];
+	float suma = 0;
+	for (int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++) {
+		printf("Introduce un numero: ");
+		scanf("%f", &arr[i]);
+		suma += arr[i];
+	}
+	float sizearr = (sizeof(arr)/sizeof(arr[0]));
+	float media = suma / sizearr;
+	printf("La media de los numeros es: %f", media);
+	return 0;
+}
