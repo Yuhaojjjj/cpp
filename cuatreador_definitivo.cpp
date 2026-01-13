e6231a8bff7de54d6acc393bed78e1870ae2efda
Yuhaojjjj <ye.yuh24@zaragoza.salesianos.edu>
2026-01-13 11:42:15 +0100

    Add files via upload

---
+++ cuatreador_definitivo.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+	int arr[4];
+	int suma = 0;
+	int max = arr[0];
+	for (int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++) {
+		printf("Introduce un numero: ");
+		scanf("%i", &arr[i]);
+		int nn = arr[i];
+		if (max < nn) {
+			max = arr[i];
+		suma += arr[i];
+		}
+	}
+	printf("La suma de los numeros es: %i\n", suma);
+	printf("El numero mayor es: %i", max);
+	return 0;
+}
