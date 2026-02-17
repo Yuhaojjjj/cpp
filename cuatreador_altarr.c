f8c9a06af2e824d59a368208f78f6d8ff2aca7e7
Yuhaojjjj <ye.yuh24@zaragoza.salesianos.edu>
2026-02-17 11:21:15 +0100

    Correct variable usage and loop structure
    Fixed variable declaration and loop index in the second loop.

--- cuatreador_altarr.c
+++ cuatreador_altarr.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 int main() {
 	int arr[20];
-	for (int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++) {
+	int i
+	for (i = 0; i < sizeof(arr)/sizeof(arr[0]); i++) {
 		printf("Introduce un numero: ");
 		scanf("%i", &arr[i]);
 	}
 	int max = arr[0];
-	for (int j = 0; j < sizeof(arr)/sizeof(arr[0]); j++){
-		int nn = arr[j];
+	for (i = 0; j < sizeof(arr)/sizeof(arr[0]); i++){
+		int nn = arr[i];
 		if (max < nn) {
-			max = arr[j];
+			max = arr[i];
 		}
 	}
 	printf("El numero mayor es: %i", max);
 	return 0;
 }
+
