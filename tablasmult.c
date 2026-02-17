fbfa0534eeef292ab1f61a8b32431bc09aa8892d
Yuhaojjjj <ye.yuh24@zaragoza.salesianos.edu>
2026-02-17 11:27:21 +0100

    Update and rename tablasmult.cpp to tablasmult.c

--- tablasmult.c
+++ tablasmult.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
 int main () {
-	for (int mult = 0; mult <= 10; mult++) {
-		for (int i = 0; i <= 10; i++) {
+	int mult;
+	int i;
+	for (mult = 0; mult <= 10; mult++) {
+		for (i = 0; i <= 10; i++) {
 			printf("%d * %d = %d\n", mult, i, mult * i);
 		}
 	}
 	return 0;
 }
+
