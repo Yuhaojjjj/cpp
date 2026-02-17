043928214474781775aedaa1c17e6db171e99dd2
Yuhaojjjj <ye.yuh24@zaragoza.salesianos.edu>
2026-02-17 11:20:21 +0100

    Correct loop variable for printing characters
    Fix variable declaration and loop iteration for printing.

--- array_letras.c
+++ array_letras.c
@@ -2,12 +2,14 @@
 
 int main() {
 	char string[4];
-	for (int i = 0; i < 4; i++) {
+	int i = 0;
+	for (i = 0; i < 4; i++) {
 		scanf("%c", &string[i]);
 	}
-	for (int j = 0; j < 4; j++) {
+	for (i = 0; j < 4; j++) {
 		printf(" %c ", string[j]);
 	}
 	return 0;
 }
+
 
